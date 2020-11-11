497e43c1e5a15cd917f7c8828cd73b4cfd21b60b
EnkhErdene0421 <enkhgem@gmail.com>
2020-11-11 12:58:31 +0800

    merge

---
+++ modal.js
@@ -0,0 +1,32 @@
+var modal = document.getElementById('loginModal');
+var registerModal = document.getElementById('registerModal');
+
+function openLoginForm() {
+ document.getElementById("loginModal").style.display = "block";
+ document.getElementById("registerModal").style.display = "none";
+ }
+
+ function closeLoginForm() {
+ document.getElementById("loginModal").style.display = "none";
+ }
+
+ function openRegisterForm() {
+ document.getElementById("registerModal").style.display = "block";
+ document.getElementById("loginModal").style.display = "none";
+ }
+
+ function closeRegisterForm() {
+ document.getElementById("registerModal").style.display = "none";
+ }
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
+ window.onclick = function(event) {
+     if (event.target == modalregisterModal) {
+         registerModal.style.display = "none";
+     }
+ }
